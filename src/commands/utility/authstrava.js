2c0151c84250fe7700ecf45e4bbd5b08f4004184
Kristian <kristianhadberg@gmail.com>
2024-02-23 17:38:26 +0100

    auth bot command now sends a button with the authentication link

--- src/commands/utility/authstrava.js
+++ src/commands/utility/authstrava.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  Buttonstyle,
+  ButtonStyle,
+} = require("discord.js");
 const { stravaClientId, appUrl } = require("../../../config.json");
 
 module.exports = {
@@ -11,6 +17,16 @@ module.exports = {
     const auth_link = "https://www.strava.com/oauth/token";
     const stravaAuthLink = `https://www.strava.com/oauth/authorize?client_id=${stravaClientId}&response_type=code&redirect_uri=${appUrl}/exchange_token&approval_prompt=force&scope=activity:read_all,activity:write`;
 
-    await interaction.reply(`Please click: ${stravaAuthLink}`);
+    const authButton = new ButtonBuilder()
+      .setLabel("Connect")
+      .setURL(stravaAuthLink)
+      .setStyle(ButtonStyle.Link);
+
+    const row = new ActionRowBuilder().addComponents(authButton);
+
+    await interaction.reply({
+      content: "Please click to connect to Strava.",
+      components: [row],
+    });
   },
 };
